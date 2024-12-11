c3f9aa60b115cb9c2f92b8a23c2b22c97abdd5f2
moodynooby <98732831+moodynooby@users.noreply.github.com>
2024-12-11 19:47:16 +0530

    migration

---
+++ src/main.vue
@@ -0,0 +1,12 @@
+  const app = Vue.createApp({
+   data(numPlay) {
+    return {
+        intPage : true,
+        numPlay : '0',
+        manPage : false, 
+    }
+   }
+  })
+  
+  app.mount('#app')
+
